b13d1a28de304ecf52159724120bc6255132d2bd
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-25 21:55:03 +0900

    feat: monitoring 작성

---
+++ monitoring/src/main/java/com/monitoring/api/HealthController.java
@@ -0,0 +1,13 @@
+package com.monitoring.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Service is running!";
+    }
+}
