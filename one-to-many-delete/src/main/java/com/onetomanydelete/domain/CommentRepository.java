a6167272c52c95f74db5668e54278425cf40a8a5
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-22 00:46:20 +0900

    feat: relation delete 작성

--- one-to-many-delete/src/main/java/com/onetomanydelete/domain/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.onetomanydelete.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- one-to-many-delete/src/main/java/com/onetomanydelete/domain/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.onetomanydelete.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
